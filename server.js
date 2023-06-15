982c5ff32c3003f27681cb6a767dc5c3ff32bf8a
Akshat <iamakku0.0.0.1@gmail.com>
2023-06-15 13:13:40 +0530

    commiting on 15 june 23

--- server.js
+++ server.js
@@ -7,18 +7,23 @@ const server = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 
-//parsing request data from json format to js object by middleware
+//parsing request data from json format to js object by middleware , server use krega express.js ko
 server.use(express.json());
 
-const routes = require("./routes/blogRoute");
+const blog = require("./routes/blog");
 
-server.use("akshat/Blogs",routes);
+//mounting , and mapping with route
+server.use("/akshat",blog);
 
 server.listen(PORT,()=>{
     console.log("SERVER STARTED");
 });
 
-const dbconnect = require("./config/dbconnect");
+const dbconnect = require("./config/db");
 
 dbconnect();
+
+server.get("/",(res,req)=>{
+    res.setEncoding("server default page")
+});
 
